66fd41e0afcb59afcba25afff603ece88a7baa0a
Imran <imranq02@gmail.com>
2020-08-16 02:10:02 -0400

    Finished with 86% accuracy kdtrees. Added seamcarver

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -16,6 +16,10 @@ Why is a kdtree so efficient?
 Start with vertical
 Which data should you start with? Does it work best with center, out? (Ranked by Euclidean distance from the center)
 
+Future
+- Put the type of node in the Node class (either horizontal or vertical)
+- Handle curvature of the surface
+- Handle moving points (kinetic)
 */
 
 
@@ -57,19 +61,44 @@ public class KdTree {
 	}
 
 	public Iterable<Point2D> range(RectHV rect) {
-		Queue<Point2D> result = new Queue<Point2D>();
-		return result;
+		if (rect == null) {
+			throw new IllegalArgumentException("Rect is null")
+		}
+		
+		Queue<Point2D> q = new Queue<Point2D>();
+		range(rect, root, q);
+		return q;
+	}
+
+	/*
+		Given a node and a rect, we check whether the node
+		is in the rectangle by checking whether its distance is zero.
+		In that case, we return the point
+		
+
+		if intersects the positive part of the node, we go 
+		to the 
+	*/
+	private void range(RectHV rect, Node node, Queue<Point2D> q) {
+		if (node == null) {
+			return;
+		}
+
+		if (rect.contains(node.p)) {
+			q.enqueue(node.p);
+		}		
+		
+		if (rect.intersects(node.rect)) {
+			range(rect, node.right, q);
+			range(rect, node.left, q);
+		} else {
+			return;
+		}
+		return;
 	}
 
 	public boolean isEmpty() {
 		return size == 0;
-	}
-
-	public Point2D nearest(Point2D p) {
-		//find the nearest Point in the tree that's close to p
-		//traverse through the partitions based on vertical or not
-		//keep track of the euclidean distances at each tree level (when one side is greater, ignore the other side)
-		return nearest(root, p, true, root.p);
 	}
 
 	public int size() {
@@ -99,26 +128,30 @@ public class KdTree {
 		}
 		draw(node.left, !vertical);
 		draw(node.right, !vertical);
-
-
 	}
 
 	/*
-	* Traverse through the tree going vertical and horizontal until we reach the end
-	*
+	
+	Traverse through the tree going vertical and horizontal nodes until we reach the end
+	start with vertical nodes
+
+
+
 	*/
 
 	public void insert(Point2D p) {
 		//use helper function to recursively insert a value
-		root = insert(root, p, true, 0, 0, 1, 1, 0);
+		root = insert(root, p, true, 0, 0, 1, 1);
 	}
 
-	public boolean contains(edu.princeton.cs.algs4.Point2D p) {
-		//use helper function to recursively insert a value
-		return contains(root, p, true);
-	}
 
-	private Node insert(Node node, Point2D p, boolean vertical, double x1, double y1, double x2, double y2, int level) {
+	/*
+		Helper function for insertion
+		Recursive insert. Once we get to the end of the tree, we return a new node
+		Keep track of the current rectangle we are associated with
+	*/
+
+	private Node insert(Node node, Point2D p, boolean vertical, double x1, double y1, double x2, double y2) {
 		if (node == null) {
 			size++;
 			return new Node(p, new RectHV(x1, y1, x2, y2));
@@ -129,37 +162,28 @@ public class KdTree {
 			return node;
 		}
 
-		//go left based on x value
-		StdOut.println("");
-		for (int i = 0; i<level; i++) {
-			StdOut.print(" ");
-		}
-
 		double cmp = 0;
 		if (vertical) {
 			cmp = p.x() - node.p.x(); //go left if negative and right if positive
-			StdOut.print("Vertical");
-			if (cmp > 0) { //go to right
-				StdOut.print("- going to the right");
-				node.right = insert(node.right, p, !vertical, node.p.x(), y1, x2, y2, level+1);	
+			if (cmp > 0) { //go to right, with rectangle partitioned by node x value
+				node.right = insert(node.right, p, !vertical, node.p.x(), y1, x2, y2);	
 			} else { //go to left
-				StdOut.print("- going to the left");
-				node.left = insert(node.left, p, !vertical, x1, y1, node.p.x(), y2, level+1);	
+				node.left = insert(node.left, p, !vertical, x1, y1, node.p.x(), y2);	
 			}
-			
-		} else {
-			StdOut.print("Horizontal");
+		} else { //horizontal node, compare y-values
 			cmp = p.y() - node.p.y();
-			if (cmp > 0) { //go up
-				StdOut.print("- going to the top");
-				node.right = insert(node.right, p, !vertical, x1, node.p.y(), x2, y2, level+1);	
+			if (cmp > 0) { //go up with node partitioned by node y value
+				node.right = insert(node.right, p, !vertical, x1, node.p.y(), x2, y2);	
 			} else { //go down
-				StdOut.print("- going to the bottom");
-				node.left = insert(node.left, p, !vertical, x1, y1, x2, node.p.y(), level+1);	
+				node.left = insert(node.left, p, !vertical, x1, y1, x2, node.p.y());	
 			}
 		}
-		StdOut.println("");
 		return node;
+	}
+
+	public boolean contains(Point2D p) {
+		//use helper function to recursively insert a value
+		return contains(root, p, true);
 	}
 
 	private boolean contains(Node node, Point2D p, boolean vertical) {
@@ -183,33 +207,53 @@ public class KdTree {
 			}
 			// return false;
 		}
-
 	}
 
-	private Point2D nearest(Node node, Point2D p, boolean vertical, Point2D c) {
-		Point2D closestPoint = c;
+	/*
+		This is the recursive function to find the nearest point p
+		within the KdTree. We find the nearest point in a unique way
+		not by comparing each value with each other, but by partitioning
+		the region. The first point becomes the node and then futher insertions
+		go to the left if node y-value is greater or to the right if the node x-value 
+		is greater. The next insertion we switch to x.
 
-		if (node == null) {
-			return c;
-		}
+		Now given any arbitrary point, we calculate the nearest point by iterating
+		through the tree. If the current node is a vertical node, we compare x values to 
+		see whether we go left or right. If it is a horizontal node, we compare y values.
 
+		We keep going through the tree until we reach the end, while keeping the minimum point
+		stored. Once we reach a null value, we send back the minimum 
+	*/
 
-		if (node.p.distanceSquaredTo(p) < node.p.distanceSquaredTo(closestPoint)) {
-			closestPoint = node.p;
-		}
-
-		//check the subtrees 
-		if (node.rect.distanceSquaredTo(p) < closestPoint.distanceSquaredTo(p)) {
-
+	public Point2D nearest(Point2D p) {
+		//find the nearest Point in the tree that's close to p
+		//traverse through the partitions based on vertical or not
+		//keep track of the euclidean distances at each tree level (when one side is greater, ignore the other side)
+		if (p == null) {
+			throw new IllegalArgumentException("Given point is null");
 		}
 		
-		double cmp_l = p.distanceSquaredTo(node.left.p);
-		double cmp_r = p.distanceSquaredTo(node.right.p);
+		//starts out as a vertical node, with closest point as the root
+		return nearest(root, p, root.p);
+	}
 
-		if (cmp_l > cmp_r) {
-			closestPoint = nearest(node.left, p, !vertical, closestPoint);
-		} else {
-			closestPoint = nearest(node.right, p, !vertical, closestPoint);
+	private Point2D nearest(Node node, Point2D p, Point2D closestPoint) {
+		//reached the end of the tree, return the closest point
+		if (node != null) {
+			//rare, but possible that none of the subtree points are closer than the current previous point
+			if (closestPoint.distanceSquaredTo(p) >= node.rect.distanceSquaredTo(p)) {
+				if (node.p.distanceSquaredTo(p) < closestPoint.distanceSquaredTo(p)) {
+					closestPoint = node.p;
+				}
+
+				if (node.right != null && node.right.rect.contains(p)) {
+					closestPoint = nearest(node.right, p, closestPoint);
+					closestPoint = nearest(node.left, p, closestPoint);
+				} else {
+					closestPoint = nearest(node.left, p, closestPoint);
+					closestPoint = nearest(node.right, p, closestPoint);
+				}
+			}
 		}
 		return closestPoint;
 	}
